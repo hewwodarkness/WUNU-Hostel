21527478f7d11a07e10ba7707a5f5007411cb0dc
hewwodarkness <shuvalov.volodymyr@gmail.com>
2021-03-21 19:23:32 +0200

    some changes in main page

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+<body> 
+
+<div class="menu">
+
+    <div class="logo">
+
+    </div>
+
+    <div class="menu-info">
+
+        <div>
+            телефон
+        </div>
+
+        <div>
+            |
+        </div>
+
+        <div>
+            Адреса
+        </div>
+
+    </div>
+
+    <div class="hamburger">
+    
+    </div>
+</div>
+
+<div class="main">
+
+    <div class="main-left-side">
+        <div class="main-bigtext">
+            Sit consectetur id labore adipisicing consequat anim veniam.
+        </div>
+
+        <div class="main-smalltext">
+            Sit consectetur id labore adipisicing consequat anim veniam.
+        </div>
+
+        <button class="main-button">
+            More info
+        </button>
+
+        <div class="main-info-bottom">
+
+        </div>
+    </div>
+
+    <div class="main-right-side">
+        <img class="img-rs" src="3.png">
+    </div>
+</div>
+    
+
+
+
+
+</body>
+</html>
