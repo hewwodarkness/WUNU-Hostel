eb6ee26cf5d46013448513ac6b1e3913cbc3c1fa
hewwodarkness <shuvalov.volodymyr@gmail.com>
2021-04-12 12:51:11 +0300

    big update for main page

--- index.php
+++ index.php
@@ -5,63 +5,85 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,300&display=swap" rel="stylesheet">
+    <title>IOSU</title>
 </head>
 <body> 
 
 <div class="menu">
+    <div class="menu-child">
 
-    <div class="logo">
-        <img class="img-logo" src="4.png">
-    </div>
-
-    <div class="menu-info">
-
-        <div>
-            телефон
+        <div class="logo">
+            <img class="img-logo" src="4.png">
         </div>
 
-        <div>
-            |
+        <div class="nav">
+
+            <div class="nav-child">
+                <div class="nav-child-dot">
+                    gwegewrg
+                </div>
+                <div class="nav-child-dot">
+                    gwegewrg
+                </div>
+                <div class="nav-child-dot">
+                    gwegewrg
+                </div>
+                <div class="nav-child-dot">
+                    gwegewrg
+                </div>
+            </div>
+
         </div>
-
-        <div>
-            Адреса
-        </div>
-
-    </div>
-
-    <div class="hamburger">
-    
     </div>
 </div>
 
 <div class="main">
+    <div class="main-child">
 
-    <div class="main-left-side">
-        <div class="main-bigtext">
-            Sit consectetur id labore adipisicing consequat anim veniam.
+        <div class="text">
+            <div class="hero-banner__content text-center">
+                <h1>We Well Understand Your Intention</h1>
+                <a class="button" href="#">Make an Appoinment</a>
+            </div>
         </div>
 
-        <div class="main-smalltext">
-            Sit consectetur id labore adipisicing consequat anim veniam.
-        </div>
-
-        <button class="main-button">
-            More info
-        </button>
-
-        <div class="main-info-bottom">
-
-        </div>
-    </div>
-
-    <div class="main-right-side">
-        <img class="img-rs" src="3.png">
+        
     </div>
 </div>
-    
+<div class="ss">
+    <div class="ee">
+        <div class="row">
 
+            <div class="static__single">
+                <h3>35</h3>
+                <h4>Dedicated Lawyer</h4>
+            </div>
+
+
+            <div class="static__single">
+                <h3>1526</h3>
+                <h4>Sucessful Cases</h4>
+            </div>
+
+
+            <div class="static__single">
+                <h3>720</h3>
+                <h4>Satisfied Client</h4>
+            </div>
+
+
+            <div class="static__single">
+                <h3>680</h3>
+                <h4>Personal Cases</h4>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+ 
 
 
 
